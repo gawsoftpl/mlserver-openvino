8f6e9738bbdf5026d6c8c8ccc9ad9185abbea15a
Pitos <contact@gawsoft.com>
2023-09-24 17:54:55 +0200

    Upgrade versions

--- mlserver_openvino/convert.py
+++ mlserver_openvino/convert.py
@@ -1,6 +1,27 @@
 import sys
 from mlserver_openvino import OpenvinoRuntime
+import os
+import getopt
+
+def _convert_onnx_to_openvino(path_in, path_out):
+    OpenvinoRuntime.convert_onnx(path_in, path_out)
+
+def convert_onnx(path_in, path_out):
+    if os.path.isdir(path_in):
+        abs_path = os.path.abspath(path_in)
+        save_path = os.path.dirname(abs_path)
+        for file_path in glob.glob(os.path.join(path_in, '**/*.onnx'), recursive=True):
+            _convert_onnx_to_openvino(file_path, save_path)
+    else:
+        _convert_onnx_to_openvino(path_in, path_out)
 
 if __name__ == '__main__':
+    options, args = getopt.getopt(sys.argv, "")
+
     print("Start converting")
-    OpenvinoRuntime.convert_onnx(sys.argv[1], sys.argv[2])
+    path_in = sys.argv[1]
+    path_out = ""
+    if len(args) > 2:
+        path_out = args[2]
+
+    _convert_onnx_to_openvino(path_in, path_out)
